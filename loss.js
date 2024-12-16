4b62c54cdd74b7cb7be6cd12ad4eb7b6a37f6a85
beatopia <89479623+beatopia@users.noreply.github.com>
2024-12-16 07:36:00 -0800

    Update loss.js

--- loss.js
+++ loss.js
@@ -10,16 +10,23 @@ const messages = [
 let clickCount = 0;
 
 button.addEventListener("click", () => {
+  //listening for a click of the button
   if (clickCount < messages.length) {
+    //if clickcount is less than messages length
     button.textContent = messages[clickCount];
+    //make the button display the next message in the messages array
     const scale = 1 - 0.2 * clickCount;
+    //make button smaller
     button.style.transform = `scale(${scale}) translateY(${clickCount * 10}px)`;
+    //translate it so it moves
 
     clickCount++;
+    //iterate click count
   }
 
   if (clickCount === messages.length) {
     document.body.style.backgroundImage = "url(images/friend.jpg)";
+    //if you get to the end of the messages, it jump scares you
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundPosition = "center";
     button.style.display = "none";
@@ -29,10 +36,12 @@ button.addEventListener("click", () => {
     setTimeout(() => {
       window.location.href = "index.html";
     }, 2500);
+    //after 2.5 seconds of jumpscare, it returns you back to the maze
   }
 });
 var histheme = new Audio("sounds/histheme.mp3");
 window.onload = function () {
   histheme.play();
   histheme.volume = 0.5;
+  //play music onload
 };
